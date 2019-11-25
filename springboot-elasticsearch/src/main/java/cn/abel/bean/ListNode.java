6fd6e5677ecbb957bd7c4a127307f8d4b0d209ca
zangliulu <zangliulu@sdxnetcafe.com>
2019-11-25 09:24:43 +0800

    测试代码

---
+++ springboot-elasticsearch/src/main/java/cn/abel/bean/ListNode.java
@@ -0,0 +1,7 @@
+package cn.abel.bean;
+
+public class ListNode {
+    int val;
+    ListNode next;   // 下一个链表对象
+    ListNode(int x) { val = x; }  //赋值链表的值
+}
--- springboot-elasticsearch/src/test/java/cn/abel/service/ServiceTest.java
+++ springboot-elasticsearch/src/test/java/cn/abel/service/ServiceTest.java
@@ -1,15 +1,16 @@
 package cn.abel.service;
 
 import cn.abel.BaseTest;
+import cn.abel.bean.ListNode;
 import cn.abel.bean.User;
 import cn.abel.util.ElasticsearchUtil;
 import cn.abel.util.RedisUtil;
 import cn.abel.vo.EsEntity;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 
 /**
@@ -39,8 +40,26 @@ public class ServiceTest extends BaseTest {
 
         //userService.addUser(user);
         // userService.getUserById("1");
-        redisUtil.set("cesi","1111111111111111");
+       /* List<User> users  = new ArrayList<>();
+        int i = 0;
+        while (i<3){
+            User user = new User();
+            user.setId(i);
+            user.setName("cesi"+i);
+            users.add(user);
+            i++;
+        }
+        for (User u:users){
+            u.setAddress("测试地址");
+        }*/
+       /* int[] nums = new int[2];
+        nums[0] = 3;
+        nums[1] = 3;
+        int target = 6 ;
+        int[] integers = this.twoSum(nums,target);
+        System.out.println(JSONObject.toJSONString(integers));*/
 
+        System.out.println(getMaxStr(121));
 
     }
 
@@ -67,5 +86,118 @@ public class ServiceTest extends BaseTest {
 
         }
         return sb.toString();
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+        Set<Integer> set = new HashSet<Integer>();
+        Map<Integer,Integer> nummap = new HashMap<>();
+        for (int i=0 ; i<nums.length;i++){
+            nummap.put(i,nums[i]);
+        }
+        for (int i=0 ; i<nums.length;i++){
+            if( nummap.containsKey(i)){
+                for ( Map.Entry<Integer,Integer> e :nummap.entrySet()) {
+                    if(e.getKey() != i){
+                        int tar = e.getValue() + nums[i];
+                        if(target == tar){
+                            set.add(i);
+                        }
+                    }
+
+                }
+            }
+        }
+        int[] r  = new int[set.size()];
+        int size = 0;
+        for (Integer i :set){
+            if(size <= set.size()){
+                r[size] = i;
+                size++;
+            }
+        }
+        return r;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null ;
+    }
+
+    private int getNum(int x) {
+        int temp = 0;
+        int len = String.valueOf(Math.abs(x)).length();
+        if(len>10){
+            return 0;
+        }
+        while (x != 0) {
+            int pop = x % 10;
+            System.out.println(pop+"+-----+"+temp);
+            if(x>0){
+                if(Integer.MAX_VALUE-temp * 10>0){
+                    temp = temp * 10 + pop;
+                    x = x / 10;
+                }else {
+                    return 0;
+                }
+            }else if(x < 0){
+                if(Integer.MIN_VALUE+temp * 10 < 0){
+                    temp = temp * 10 + pop;
+                    x = x / 10;
+                }else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
+        }
+        return temp;
+    }
+
+    private  int getNumLenght(int x) throws Exception {
+        try {
+            int un = x;
+            ArrayList<Integer> s = new ArrayList<>();
+            //x = x>0?x:-x;
+            int len = String.valueOf(x).length();
+            Double a = Double.valueOf("" + x);
+            for (int i =1;i<=len;i++ ){
+                Double v = a % new Double("10");
+                s.add(v.intValue());
+                a = (a-v)/10;
+            }
+            String str = new String();
+            for (int i = 0 ; i<s.size();i++){
+                str=str+s.get(i);
+            }
+            if(un<0){
+                return -Integer.valueOf(str);
+            }else {
+                return Integer.valueOf(str);
+            }
+        } catch (NumberFormatException e) {
+            throw new Exception("超出整最大值");
+        }
+
+    }
+
+
+    private Boolean getMaxStr (int x){
+        List<Character> strList = new ArrayList<>();
+        String s = String.valueOf(x);
+        for (int i = 0 ;i<s.length();i++){
+            strList.add(s.charAt(i));
+        }
+        String s1 = new String();
+        for (int i = strList.size()-1 ;i>=0;i--){
+            s1 =s1+strList.get(i);
+        }
+        if(s.equals(s1)){
+            return true;
+        }else {
+            return false;
+        }
+
+
     }
 }
