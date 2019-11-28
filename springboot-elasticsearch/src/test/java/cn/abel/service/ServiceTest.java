38e18f8609e5b80101fe42a8a80bbfacd9f43978
zangliulu <zangliulu@sdxnetcafe.com>
2019-11-28 09:45:12 +0800

    spring boot docker

--- springboot-elasticsearch/src/test/java/cn/abel/service/ServiceTest.java
+++ springboot-elasticsearch/src/test/java/cn/abel/service/ServiceTest.java
@@ -58,8 +58,9 @@ public class ServiceTest extends BaseTest {
         int target = 6 ;
         int[] integers = this.twoSum(nums,target);
         System.out.println(JSONObject.toJSONString(integers));*/
+        String s ="MMMCMXCIX";
 
-        System.out.println(getMaxStr(121));
+        System.out.println(this.getLuomaToNumber(s));
 
     }
 
@@ -197,7 +198,35 @@ public class ServiceTest extends BaseTest {
         }else {
             return false;
         }
+    }
 
+    private Integer getLuomaToNumber(String s){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
 
+        int ans = 0;
+        for(int i = 0;i < s.length();) {
+            System.out.println( s.substring(i, i+2));
+            if(i + 1 < s.length() && map.containsKey(s.substring(i, i+2))) {
+                ans += map.get(s.substring(i, i+2));
+                i += 2;
+            } else {
+                ans += map.get(s.substring(i, i+1));
+                i ++;
+            }
+        }
+        return ans;
     }
 }
